432bd09ea07f07bcd48eb8630279791fa68d2786
Ben <develop@odin-inc.com>
2013-03-11 10:43:07 -0400

    Initial Commit

---
+++ Tiling/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Tiling
+//
+//  Created by Ken Thomsen on 3/11/13.
+//  Copyright (c) 2013 Ben McCloskey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
